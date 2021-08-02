a0f34431652de2edb4a6eeaf764e0c20637326f2
akira <akira@alterra.id>
2021-08-02 12:19:55 +0700

    menambahkan login dan register alise

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+import { setHeaderToken } from '../utils/auth'
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+axios.defaults.baseURL = 'http://localhost:8000/api/'
+
+Vue.config.productionTip = false
+
+const token = localStorage.getItem('token');
+
+if (token) { 
+  setHeaderToken(token) 
+} 
+
+store.dispatch('get_user', token)
+.then(() => {
+  new Vue({
+    router,
+    store,
+    render: h => h(App)
+  }).$mount('#app')
+}).catch((error) => {
+  console.error(error);
+})
